665199fd641f9eb3e8a851acce23454cb1af91a3
Vitor Soares <pereiravitor1218@gmail.com>
2023-01-18 16:08:33 -0300

    feat[req 6]: busca pelo titulo

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,13 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news_list = search_news({"title": {"$regex": title, "$options": "i"}})
+    news_formated_list = []
+    for news in news_list:
+        news_formated_list.append((news["title"], news["url"]))
+    return news_formated_list
 
 
 # Requisito 7
