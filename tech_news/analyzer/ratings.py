da9197fb218dcc3b39a0e0b421358ed439b1c12c
Vitor Soares <pereiravitor1218@gmail.com>
2023-01-18 17:01:09 -0300

    feat[req 10]: top 5 noticias

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,20 @@
+from tech_news.database import search_news
+
+
+def order_by_comment(e):
+    return e["comments_count"]
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news_list = search_news({})
+    news_list.sort(key=order_by_comment, reverse=True)
+    if len(news_list) > 5:
+        news_list = news_list[:5]
+    list_formated = []
+    for news in news_list:
+        list_formated.append((news["title"], news["url"]))
+    return list_formated
 
 
 # Requisito 11
